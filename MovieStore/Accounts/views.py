1b83b70a5689eb55ef55b1f4e3555eb8f504faf1
Giorgi Okreshidze <okreshidze.giorgi@kiu.edu.ge>
2024-11-27 14:23:02 +0400

    Implemented Login functionality

--- MovieStore/Accounts/views.py
+++ MovieStore/Accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
+from django.contrib.auth import login as auth_login, authenticate
 
 # Create your views here.
 def signup(request):
@@ -13,7 +14,25 @@ def signup(request):
         form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
-            return redirect('home.index')
+            return redirect('accounts.login')
         else:
             template_data['form'] = form
             return render(request, 'accounts/signup.html', {'template_data' : template_data})
+        
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html', {'template_date' : template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            template_data['error'] = 'the username or password is incorrect.'
+            return render(request, 'accounts/login.html', {'template_data' : template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
